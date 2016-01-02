ae5953f96cdc23b48eea8723d5b6f53e502531a8
QiannanXu <qnxu@thoughtworks.com>
2016-01-02 22:12:39 +0800

    Generate a number when game is inited.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,8 +1,13 @@
 $(function() {
-    var grid = document.querySelectorAll(".grid");
+    var gridElem = document.getElementsByClassName("grid")[0];
+    var touchElem = document.getElementsByClassName("touch")[0];
+    var scoreElem = document.getElementsByClassName("score")[0];
+
+    var score = 0, grid;
 
     var initGrid = function() {
         generateNumber();
+        updateGrid();
     };
 
     var generateNumber = function () {
@@ -11,9 +16,39 @@ $(function() {
         var x, y, possibles = [];
 
         for(x = 0; x < 4; x++) {
-            for(x = 0; x < 4; x++) {
-                if(grid[y][x] ===  -1) {
+            for(y = 0; y < 4; y++) {
+                if(grid[x][y] ===  -1) {
                     possibles.push([x, y]);
+                }
+            }
+        }
+
+        if(possibles.length) {
+            var randomBlock = possibles[(Math.floor(Math.random() * possibles.length))],
+                x = randomBlock[0],
+                y = randomBlock[1];
+
+            grid[x][y] = 2;
+        } else {
+            //if(!checkMoveable()) {
+            //    //gameover();
+            //}
+        }
+    };
+
+    var updateGrid = function() {
+        var e, x, y;
+
+        for(x = 0; x < 4; x++) {
+            for(y = 0; y < 4; y++) {
+                e = gridElem.getElementsByTagName("div")[(x * 4) + y];
+
+                if(grid[x][y] !== -1) {
+                    e.innerHTML = grid[x][y];
+                    e.setAttribute("class", "b" + grid[x][y]);
+                } else {
+                    e.innerHTML = "";
+                    e.setAttribute("class", "bv");
                 }
             }
         }
